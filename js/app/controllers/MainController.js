78e897d5d9609cde57bd2c7dad6588758a539738
cernanb <cernanb@gmail.com>
2016-05-23 07:29:08 -0700

    used karma and jasmine to test controller

--- js/app/controllers/MainController.js
+++ js/app/controllers/MainController.js
@@ -1,7 +1,11 @@
 function MainController() {
-    this.name = 'Bill Gates';
+    this.name = 'Steve Jobs';
 }
 
 angular
     .module('app')
     .controller('MainController', MainController);
+
+function addNumbers(numOne, numTwo){
+  return parseFloat(numOne, 10) + parseFloat(numTwo, 10);
+}
